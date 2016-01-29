9b5cfc4ce8251cc143b344c5475984855c1c490a
Rao (Katie) Xu <dnait@users.noreply.github.com>
2016-01-29 12:36:30 -0500

    Update immuViewController.m

--- immuViewController.m
+++ immuViewController.m
@@ -11,26 +11,16 @@
 #import "Immu.h"
 
 
-@interface immuViewController ()
-
-{
+@interface immuViewController () {
     NSArray *immus;
 }
 @property (nonatomic, strong) UISearchController *searchController;
-
 @property (strong, nonatomic) NSMutableArray *searchResults;
-
-
 @end
-
-
-
-
 
 @implementation immuViewController
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
         // Custom initialization
@@ -38,8 +28,7 @@
     return self;
 }
 
-- (void)viewDidLoad
-{
+- (void)viewDidLoad {
     [super viewDidLoad];
     //initialize the searchController
     _searchController = [[UISearchController alloc] initWithSearchResultsController:nil];
@@ -55,133 +44,91 @@
     immu1.who = @"11 to 26";
     immu1.frequency=@"One series of 3 vaccines";
 
-    
     Immu *immu2 = [Immu new];
     immu2.name = @"Influenza Vaccine";
     immu2.who =@"6 months and over";
     immu2.frequency = @"Every year";
 
-    
     Immu *immu3 = [Immu new];
     immu3.name = @"Pneumococcal Vaccine";
     immu3.who = @"65 and over\n\n\nAny age if smoker, diabetes, cancer, or heart, lung, immune disease";
     immu3.frequency = @"Single vaccination\n\n\nSingle vaccination followed by single revaccination in 5 years";
 
-    
     Immu *immu4 = [Immu new];
     immu4.name = @"Diphtheria/Tetanus/Pertussis Vaccine";
     immu4.who = @"19 to 64";
     immu4.frequency = @"Single vaccination in lieu of Diphtheria/Tetanus booster";
-   
     
     Immu *immu5 = [Immu new];
     immu5.name = @"Diphtheria/Tetanus Vaccine";
     immu5.who = @"Up to 65\n\n\n65 and over";
     immu5.frequency = @"Every 10 years\n\n\nSingle vaccination";
     
-    
     Immu *immu6 = [Immu new];
     immu6.name = @"Shingles Vaccine";
     immu6.who =@"60 and over";
     immu6.frequency = @"Single vaccination";
 
-
-
-   immus = [NSArray arrayWithObjects:immu1, immu2, immu3, immu4, immu5,immu6, nil];
-    
+    immus = [NSArray arrayWithObjects:immu1, immu2, immu3, immu4, immu5,immu6, nil];
     self.searchResults=[[NSMutableArray alloc] init];
-
-    
 }
 
--(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
-{
-    if (self.searchController.active)
-    {
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    if (self.searchController.active) {
         return [self.searchResults count];
-    }
-    else
-    {
+    } else {
         return [immus count];
     }
-    
-    
 }
-
--(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
-{
+ //Initializing tableView and display immu detail
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     static NSString *cellID=@"immuCell";
     UITableViewCell *cell=[tableView dequeueReusableCellWithIdentifier:cellID ];
-    if (cell == nil)
-    {
+    if (cell == nil) {
         cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:cellID];
     }
-    
-    
+
     //display immu detail
     Immu *immu = nil;
-    if (self.searchController.active)
-    {
+    if (self.searchController.active) {
         immu=[self.searchResults objectAtIndex:indexPath.row];
-    }
-    else
-    {
+    } else {
         immu = [immus objectAtIndex:indexPath.row];
-        
     }
     cell.textLabel.text=immu.name;
     cell.accessoryType=UITableViewCellAccessoryDisclosureIndicator;
     return cell;
 }
 
-
-
-
-
-
-
+//updating searchResult
 -(void)updateSearchResultsForSearchController:(UISearchController *)searchController {
-    
     NSString *searchString = [self.searchController.searchBar text];
-    
     NSPredicate *preicate = [NSPredicate predicateWithFormat:@"name CONTAINS[c] %@", searchString];
     
     if (self.searchResults!= nil) {
         [self.searchResults removeAllObjects];
     }
-    
     self.searchResults= [NSMutableArray arrayWithArray:[immus filteredArrayUsingPredicate:preicate]];
-    
     [self.immutableView reloadData];
 }
 
-    
+//Segue from the UITableViewCell of the tableViewController to a detailViewController.
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-        if ([segue.identifier isEqualToString:@"immusegue"])
-        {
-            NSIndexPath *indexPath=nil;
-            immuDetailViewController *destViewController = segue.destinationViewController;
-            Immu *immucell=nil;
-            
+    if ([segue.identifier isEqualToString:@"immusegue"]) {
+        NSIndexPath *indexPath=nil;
+        immuDetailViewController *destViewController = segue.destinationViewController;
+        Immu *immucell=nil;
+        indexPath = [self.immutableView indexPathForSelectedRow];
+        immucell = [immus objectAtIndex:indexPath.row];
 
-                indexPath = [self.immutableView indexPathForSelectedRow];
-                immucell = [immus objectAtIndex:indexPath.row];
-
-                destViewController.immucell= immucell;
-               [self.searchController setActive:FALSE];
-       }
-
+        destViewController.immucell= immucell;
+        [self.searchController setActive:FALSE];
     }
-    
+}
 
-
-
-- (void)didReceiveMemoryWarning
-{
+- (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
-
 
 @end
